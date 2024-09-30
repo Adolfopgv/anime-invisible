2fb841114f52d03b4deb4f9ddd7b52b344590719
Adolfopgv <adolfopgv@gmail.com>
2024-10-01 00:48:18 +0200

    changes

--- anime-invisible/src/pages/Home.tsx
+++ anime-invisible/src/pages/Home.tsx
@@ -2,19 +2,22 @@ export default function Home() {
   return (
     <main>
       <h1>Anime Invisible</h1>
-      <div>
+      <div className="flex flex-col">
         <span>
           ¡Bienvenid@ a tu página de confianza para hacer amigos invisibles de
           anime!
         </span>
-        <button className="btn-ghost">Crear sala</button>
-        <div className="divider">O</div>
+        <button className="btn btn-primary">Crear sala</button>
+      </div>
+      <div className="divider">o</div>
+      <div className="flex flex-col">
         <span>¡Introduce el link de tu amigo para unirte a su sala!</span>
         <input
           type="text"
           placeholder="Link de la sala"
-          className="input input-bordered w-full max-w-xs"
+          className="input input-bordered"
         />
+        <button className="btn btn-secondary">Unirse a la sala</button>
       </div>
     </main>
   );
